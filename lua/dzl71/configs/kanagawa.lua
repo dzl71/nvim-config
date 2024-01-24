72a21edac1cdc0480879d1e72e36f7c1c9b5b971
dzl71 <davidzlatin406@gmail.com>
2024-01-25 01:15:05 +0200

    added a config file for kanagawa

---
+++ lua/dzl71/configs/kanagawa.lua
@@ -0,0 +1,11 @@
+return {
+	overrides = function(colors)
+		local theme = colors.theme
+		return {
+			Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend }, -- add `blend = vim.o.pumblend` to enable transparency
+			PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+			PmenuSbar = { bg = theme.ui.bg_m1 },
+			PmenuThumb = { bg = theme.ui.bg_p2 },
+		}
+	end,
+}
