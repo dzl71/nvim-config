ee64abaebffc46370f36ff649d6a8c05c1563d58
dzl71 <davidzlatin406@gmail.com>
2024-02-06 16:26:53 +0200

    moved the keybindings to the keybindings file

--- lua/dzl71/configs/telescope.lua
+++ lua/dzl71/configs/telescope.lua
@@ -1,10 +1,3 @@
-local telescope = require("telescope.builtin")
-
-vim.keymap.set('n', '<leader>ff', function() telescope.find_files({ hidden = true }) end, {})
-vim.keymap.set('n', '<leader>gl', telescope.live_grep, {})                 -- find text across all the files in a workspace
-vim.keymap.set('n', '<leader>/', telescope.current_buffer_fuzzy_find, {}) -- find text in the current file/buffer
-vim.keymap.set('n', '<leader><Tab>', telescope.buffers, {})                -- search buffers
-vim.keymap.set('n', 'gr', telescope.lsp_references, { buffer = true })
 
 return {
 	defaults = {
