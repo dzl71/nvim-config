22fc1df90320f9ec0064137f3b1216391f4a231c
dzl71 <davidzlatin406@gmail.com>
2024-02-13 09:18:42 +0200

    formatted the file

--- lua/dzl71/plugins.lua
+++ lua/dzl71/plugins.lua
@@ -61,13 +61,13 @@ return {
 		branch = 'v2.x',
 		dependencies = {
 			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },    -- Required
-			{ 'williamboman/mason.nvim' },  -- Optional
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{ 'williamboman/mason.nvim' }, -- Optional
 			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },         -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' },     -- Required
-			{ 'L3MON4D3/LuaSnip' },         -- Required
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'L3MON4D3/LuaSnip' }, -- Required
 		},
 		config = function()
 			require("dzl71.configs.lsp")
