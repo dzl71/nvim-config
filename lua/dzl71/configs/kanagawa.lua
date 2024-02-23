ba77ec7af007f44bbbbe88bd1c0bf9c7b9cca9c1
dzl71 <davidzlatin406@gmail.com>
2024-02-23 18:23:17 +0200

    made selected completion highliting better

--- lua/dzl71/configs/kanagawa.lua
+++ lua/dzl71/configs/kanagawa.lua
@@ -1,9 +1,10 @@
 return {
+	commentStyle = { italic = false },
 	overrides = function(colors)
 		local theme = colors.theme
 		return {
 			Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend }, -- add `blend = vim.o.pumblend` to enable transparency
-			PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+			PmenuSel = { fg = "NONE", bg = "#2C4462" },                           -- bg = theme.ui.bg_p2
 			PmenuSbar = { bg = theme.ui.bg_m1 },
 			PmenuThumb = { bg = theme.ui.bg_p2 },
 		}
