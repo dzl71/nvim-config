fdfdb69bed964f53d830fa940954cd6aead19592
dzl71 <davidzlatin406@gmail.com>
2024-03-29 00:40:32 +0200

    backed up my previous rust-tools config

---
+++ lua/dzl71/backups/rust_tools-backup.lua
@@ -0,0 +1,40 @@
+local rt = require("rust-tools")
+
+return {
+	server = {
+		settings = {
+			["rust-analyzer"] = {
+				check = {
+					overrideCommand = {
+						"cargo", "clippy", "--message-format=json", -- make the linter be clippy
+						'--',
+						'-A',                     -- allow the following cases
+						'clippy::needless_return',
+					}
+				},
+				inlayHints = {
+					lifetimeElisionHints = {
+						enable = "always", -- skip_trivial
+						useParameterNames = true,
+					},
+					closingBraceHints = {
+						enable = true,
+						minLines = 0,
+					}
+				},
+			}
+		},
+		on_attach = function(_, bufnr)
+			vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+		end,
+	},
+	tools = {
+		on_initialized = function()
+			inlay_hints.set_all()
+		end,
+		inlay_hints = {
+			auto = true,
+			parameter_hints_prefix = "parameters ",
+		},
+	},
+}
