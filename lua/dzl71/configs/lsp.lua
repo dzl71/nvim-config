18f13c506bd65a7abc28759cdd45369b9ace5f85
dzl71 <davidzlatin406@gmail.com>
2024-03-29 00:44:07 +0200

    removed unnececery bindings, formatted the file, and fixed a bug

--- lua/dzl71/configs/lsp.lua
+++ lua/dzl71/configs/lsp.lua
@@ -8,7 +8,6 @@ lsp.ensure_installed({
 	'zls',
 	'clangd',
 	'jdtls',
-	--'pyright'
 	'pylsp'
 })
 
@@ -27,32 +26,78 @@ lsp.on_attach(function(client, bufnr)
 
 	local opts = { buffer = bufnr }
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "gn", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "gp", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("n", "<leader>vm", function()
-		for i = 1, 10, 1 do
-			print(i)
-		end
-	end, opts)
-	vim.keymap.set("n", "<leader>fm", function()
-		vim.lsp.buf.format({
-			format_opts = {
-				async = false,
-			},
-		})
-		vim.cmd('w')
-	end, opts)
+	vim.keymap.set("n",
+		"gd",
+		function()
+			vim.lsp.buf.definition()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"gr",
+		function()
+			vim.lsp.buf.references()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"K",
+		function()
+			vim.lsp.buf.hover()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"<leader>e",
+		function()
+			vim.diagnostic.open_float()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"gn",
+		function()
+			vim.diagnostic.goto_next()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"gp",
+		function()
+			vim.diagnostic.goto_prev()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"<leader>a",
+		function()
+			vim.lsp.buf.code_action()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"<leader>rn",
+		function()
+			vim.lsp.buf.rename()
+		end,
+		opts
+	)
+	vim.keymap.set("n",
+		"<leader>fm",
+		function()
+			vim.lsp.buf.format({
+				format_opts = {
+					async = false,
+				},
+			})
+			vim.cmd('w')
+		end,
+		opts
+	)
 end)
-
-lsp.setup()
 
 lsp.skip_server_setup({ 'rust_analyzer' })
 
+lsp.setup()
+
 vim.g.zig_fmt_autosave = 0
--- vim.g.syntastic_python_checkers = { 'mypy' }
