e9b0373327eb385246e9251929fbea97f543bdfa
dzl71 <davidzlatin406@gmail.com>
2024-05-09 12:56:28 +0300

    reformatted

--- lua/dzl71/configs/kanagawa.lua
+++ lua/dzl71/configs/kanagawa.lua
@@ -3,8 +3,13 @@ return {
 	overrides = function(colors)
 		local theme = colors.theme
 		return {
-			Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend }, -- add `blend = vim.o.pumblend` to enable transparency
-			PmenuSel = { fg = "NONE", bg = "#2C4462" },                           -- bg = theme.ui.bg_p2
+			Pmenu = {
+				fg = theme.ui.shade0,
+				bg = theme.ui.bg_p1,
+				-- add `blend = vim.o.pumblend` to enable transparency
+				blend = vim.o.pumblend,
+			},
+			PmenuSel = { fg = "NONE", bg = "#2C4462" }, -- bg = theme.ui.bg_p2
 			PmenuSbar = { bg = theme.ui.bg_m1 },
 			PmenuThumb = { bg = theme.ui.bg_p2 },
 		}
