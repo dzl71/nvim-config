6761c2d8c98597b1b18cc693e3e6ac391f0d0f0e
dzl71 <davidzlatin406@gmail.com>
2024-08-28 04:46:07 +0300

    made the completion popup darker

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -10,6 +10,20 @@ end
 
 local opts = {
 	commentStyle = { italic = false },
+	overrides = function(colors)
+		local theme = colors.theme
+		return {
+			Pmenu = {
+				fg = theme.ui.shade0,
+				bg = theme.ui.bg_p1,
+				-- add `blend = vim.o.pumblend` to enable transparency
+				blend = vim.o.pumblend,
+			},
+			PmenuSel = { fg = "NONE", bg = "#2C4462" }, -- bg = theme.ui.bg_p2
+			PmenuSbar = { bg = theme.ui.bg_m1 },
+			PmenuThumb = { bg = theme.ui.bg_p2 },
+		}
+	end,
 }
 
 return configured_kanagawa(opts)
