6dd9dcc289b0b937c8ac8dc60f91c5f090bc4e7f
dzl71 <davidzlatin406@gmail.com>
2024-09-28 23:22:53 +0300

    renamed keybindings to general

--- lua/config/keybindings/init.lua
+++ lua/config/keybindings/init.lua
@@ -1,4 +1,4 @@
-require("config.keybindings.keybindings")
+require("config.keybindings.general")
 require("config.keybindings.telescope")
 require("config.keybindings.center-navigation")
 require("config.keybindings.insert-mode-navigation")
