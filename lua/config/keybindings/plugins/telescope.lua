ae0752b902c6e5057fe16c9a4177c9213e656104
dzl71 <davidzlatin406@gmail.com>
2024-12-13 20:24:47 +0200

    removed garbage

--- lua/config/keybindings/plugins/telescope.lua
+++ lua/config/keybindings/plugins/telescope.lua
@@ -1,43 +1,34 @@
 local telescope = require("telescope.builtin")
-local telescope_actions = require("telescope.actions")
 
--- all of the keys work in normal mode
-local keys = {
+vim.keymap.set('n',
+	'<leader>ff',
+	function() telescope.find_files({ hidden = true }) end,
 	{
-		'<leader>ff',
-		function() telescope.find_files({ hidden = true }) end,
-		{
-			desc = "find files",
-		}
-	},
-	{
-		'<leader>gl',
-		telescope.live_grep,
-		{
-			desc = "find text across all the files in a workspace",
-		}
-	},
-	{
-		'<leader>/',
-		telescope.current_buffer_fuzzy_find,
-		{
-			desc = "find text in the current file/buffer",
-		}
-	},
-	{
-		'<leader><Tab>',
-		telescope.buffers,
-		{
-			desc = "search buffers",
-		}
-	},
-
-}
-
-local mappings = {
-	n = {
-		["d"] = telescope_actions.delete_buffer
+		desc = "find files",
 	}
-}
+)
 
-return { keys = keys, mappings = mappings }
+vim.keymap.set('n',
+	'<leader>gl',
+	telescope.live_grep,
+	{
+		desc = "find text across all the files in a workspace",
+	}
+)
+
+vim.keymap.set('n',
+	'<leader>/',
+	telescope.current_buffer_fuzzy_find,
+	{
+		desc = "find text in the current file/buffer",
+	}
+)
+
+vim.keymap.set('n',
+	'<leader><Tab>',
+	telescope.buffers,
+	{
+		desc = "search buffers",
+
+	}
+)
